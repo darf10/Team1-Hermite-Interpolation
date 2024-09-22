30b98bec149d30b68d83af80068d71bd8e5e8496
darf10 <118679710+darf10@users.noreply.github.com>
2024-09-22 18:10:09 +0300

    prints function and derviative

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import sympy as sp
+from sympy import *
 from sympy.utilities.lambdify import lambdify
-x = sp.sympify('x')
-e = sp.sympify('e')
-
+x = symbols('x')
 
 def Lagrange_i(data_points, i):
     """
@@ -27,11 +26,14 @@ def Lagrange_squared(data_points, i):
     :return: the i-th squared lagrange non-function
     """
     l2 = Lagrange_i(data_points, i) ** 2
-    print("Li(x)^2 =" + str(l2))
-    print()
     return l2
 
-
+def reduce_func(func):
+    reduced_func = func
+    for val in preorder_traversal(func):
+        if isinstance(val, Float):
+            reduced_func = reduced_func.subs(val, round(val, 1))
+    return reduced_func
 
 def Lagrange_Derivative(data_points, i):
     """
@@ -40,8 +42,6 @@ def Lagrange_Derivative(data_points, i):
     :return: the i-th Derivative lagrange function
     """
     dl = Lagrange_i(data_points, i).diff(x)
-    print("L'(x) = " + str(dl))
-    print()
     return lambdify(x, dl)
 
 
@@ -57,18 +57,15 @@ def Hermite_Interpolation(data_points):
         la_squared = Lagrange_squared(data_points, i)
         la_derivative = Lagrange_Derivative(data_points, i)
         hermite_function += (1 - 2*(x - xi) * la_derivative(xi)) * la_squared * yi + (x - xi)*la_squared * mi
-        print("H" + str(i) + "(x)=" + str(hermite_function) + "\n")
-
     h = hermite_function
     dh = hermite_function.diff(x)
-    print("\nH(x) = " + str(h) + "\n")
+    reduced_h = reduce_func(simplify(h))
+    reduced_dh = reduce_func(simplify(dh))
     h = lambdify(x, h)
     dh = lambdify(x, dh)
-
     for xi, yi, mi in data_points:
-        print("x=" + str(xi) + " f(x)=" + str(yi) + " f'(x)=" + str(mi) + " H(x)=" + str(h(xi)) + " H'(x)=" + str(dh(xi)))
-
-    return hermite_function
+        print("x=" + str(xi) + " f(x)=" + str(yi) + " f'(x)=" + str(mi) + " H(x)=" + str(round(h(xi), 2)) + " H'(x)=" + str(round(dh(xi), 2)))
+    return reduced_h, reduced_dh
 
 
 def Drive():
@@ -88,8 +85,13 @@ def Drive():
         if stop == "n" or stop == "N":
             break
 
-    h = Hermite_Interpolation(data_points)
+    h, dh = Hermite_Interpolation(data_points)
     print("H(x) = " + str(h))
+    print("H'(x) = " + str(dh))
+    h = lambdify(x, h)
+    dh = lambdify(x, dh)
+    input_var = int(input("\nEnter x value to calculate H(x) and H'(x):\n"))
+    print(f'H({input_var}) = {round(h(input_var), 2)}' + ", H'" + f'({input_var}) = {round(dh(input_var), 2)}')
 
 
 Drive()
